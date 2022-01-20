25591ebd6cfe81c4bdd6cd16fcac33e6824e36b0
IrinaMazurenko <irinairarara@gmail.com>
2022-01-20 15:07:40 +0200

    HW001

--- 002.py
+++
@@ -1,5 +0,0 @@
-name = 'Irina'
-print('Heloo',name)
-# This is a comment
-print('Irina')
-
---
+++ 003.py
@@ -0,0 +1,18 @@
+string_sample = "Hello world world"
+string_sample2 = "first letteR is lowErcase"
+string_sample3 = " **** extra whitespace string*"
+german_sample = "der Fluẞ"
+helo_spl = "hello"
+
+sample = '''hello'''
+
+salary = 2000
+first = 'Bob'
+last = 'Smith'
+sample = 'Helli {0} {1}. Your salary is {0}. Is your name {3}'
+print(sample.format(first, last, salary, 'London'))
+print(f'Hello {first.upper()} {last.upper()}. Your salary is {salary:.2f}')
+print(r' \n Hello World')
+
+byte_string = b' '  # unicode-table.com можно найти иероглифв
+
